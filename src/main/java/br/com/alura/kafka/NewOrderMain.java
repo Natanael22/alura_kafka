c85600fa5cf66205ca33d2ae6a736e96653712a2
Natanael22 <natanaelcorreia1@gmail.com>
2023-05-10 12:38:13 -0300

    criado mais um tipo de msg e consumidor e o servico de log

--- src/main/java/br/com/alura/kafka/NewOrderMain.java
+++ src/main/java/br/com/alura/kafka/NewOrderMain.java
@@ -1,5 +1,6 @@
 package br.com.alura.kafka;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -14,15 +15,22 @@ public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
         var producer = new KafkaProducer<String, String>(properties());
-        var value = "1,999,10.50";
+        var value = "1,888,10.50";
         var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER",value,value);
-        producer.send(record, (data, ex)->{
-            if(ex != null){
+        var email = "bem vindo! nos estamos processando seu pedido";
+        var emailRecord = new ProducerRecord<String, String>("ECOMMERCE_SEND_EMAIL", email,email);
+        producer.send(record, getCallback()).get();
+        producer.send(emailRecord, getCallback()).get();
+    }
+
+    private static Callback getCallback() {
+        return (data, ex) -> {
+            if (ex != null) {
                 ex.printStackTrace();
                 return;
             }
-            System.out.println(data.topic() + "::: partition "+ data.partition() +"/ offset "+ data.offset()+" timestamp "+data.timestamp());
-        }).get();
+            System.out.println(data.topic() + "::: partition " + data.partition() + "/ offset " + data.offset() + " timestamp " + data.timestamp());
+        };
     }
 
     private static Properties properties() {
